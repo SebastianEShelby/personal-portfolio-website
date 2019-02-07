c929005c420067fce9da4e137976d16ac3afbc0b
alex-samari <alexsamarim@gmail.com>
2019-02-06 22:25:44 -0500

    Added sticky navbar for desktop view but mobile nav is broken

---
+++ static/scripts/sticky-nav.js
@@ -0,0 +1,24 @@
+if (window.matchMedia("(min-width: 800px)").matches) {
+  // wait for the window to load and identify dom elements
+  window.onload = function() {
+    // Get the navbar
+    const navbar = document.getElementById("nav-bar");
+
+    // Get the offset position of the navbar
+    const sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function addSticky() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {
+      addSticky();
+    };
+  };
+}
