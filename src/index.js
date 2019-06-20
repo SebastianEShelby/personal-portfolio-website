8a51320e06dc6540c480c72e01c87baeb07ac009
alex-samari <alexsamarim@gmail.com>
2019-06-20 02:11:55 -0400

    Updating code for dynamic project addition

--- src/index.js
+++ src/index.js
@@ -1,4 +1,88 @@
-import { handleNavBar } from './scripts/sticky-nav';
+// import { handleNavBar } from './scripts/sticky-nav';
 import './scss/styles.scss';
 
 // handleNavBar();
+
+// const myProjects = [
+//   {
+//     projectName: 'Employee Management',
+//     projectText: 'Written using NodeJS, Express, MongoDB',
+//     imgSource: 'images/employee-management-nodejs.JPG',
+//     imgAlt: 'employee-management-nodejs'
+//   }
+// ];
+
+// const projectItems = document.getElementById('project-items');
+
+// const div = document.createElement('div');
+// let figure = document.createElement('figure');
+// let img = document.createElement('img');
+// let figcaption = document.createElement('figcaption');
+// let headlineTwo = document.createElement('h2');
+// let paragraph = document.createElement('p');
+// let anchor = document.createElement('a');
+// let icon = document.createElement('i');
+// let span = document.createElement('span');
+
+// let overlayDiv = document.createElement('div').setAttribute();
+
+// setMultipleAttributes = attributes => {
+//   for (var key in attributes) {
+//     Element.setAttribute(key, attributes[key]);
+//   }
+// };
+
+// const generateProject = () => {
+//   for (let i = 0; i < myProjects.length; i++) {
+//     const projectDiv = div.setMultipleAttributes({ class: 'project' });
+//     console.log('projectDiv', projectDiv);
+//     // const figure = figure;
+//     // const img = document.setMultipleAttributes({"src":"images/employee-management-nodejs.JPG", "alt":"employee-management-nodejs"})
+//     // const figcaption = figcaption
+//     // const headlineTwo = headlineTwo;
+//     // const paragraph = paragraph;
+//     // const anchor = anchor.setMultipleAttributes({ "class":"button is-secondary is-outlined is-rounded is-medium", "href":"#projects"})
+//     // const icon = icon.setMultipleAttributes({"class":"icon"," data-feather":"globe"})
+//     // const span = span;
+//     // const overlay = div.setMultipleAttributes({ "class":"overlay"})
+//   }
+// };
+
+// generateProject();
+
+// project.appendChild(figure);
+
+// figure.appendChild(img);
+// figure.appendChild(figcaption);
+// figure.appendChild(overlay);
+
+// figcaption.appendChild(h2)
+// figcaption.appendChild(p)
+// figcaption.appendChild(a)
+
+// a.appendChild(i)
+// a.appendChild(span)
+
+{
+  /* <div class:"project">
+              <figure>
+                <img
+                  src:"images/employee-management-nodejs.JPG"
+                  alt:"employee-management-nodejs"
+                />
+                <figcaption>
+                  <h2>Employee Management</h2>
+                  <p>
+                    Written using NodeJS, Express, MongoDB
+                  </p>
+                  <a
+                   
+                    
+                    ><i ></i>
+                    <span>See Website</span>
+                  </a>
+                </figcaption>
+                <div></div>
+              </figure>
+            </div> */
+}
