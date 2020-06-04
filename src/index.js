8e804ea17241571e1abc4c1f694c386a2432624c
Alex Samari <alexsamarim@gmail.com>
2020-06-04 09:47:59 -0400

    update comment about feather icons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { preventScrollOverFlow } from './scripts/scrollEffects';
 
 // wait for the window to load and identify dom elements
 window.onload = () => {
-  // make sure to call this function at the end to replace feather icons with svg images
+  // function replaces feather icon tags with svg images
   feather.replace();
   handleNavBar();
   preventScrollOverFlow();
