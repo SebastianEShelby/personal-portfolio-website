f02da1732d16660278d6a01367a23d960692ff26
Alex Samari <alexsamarim@gmail.com>
2020-10-26 20:38:57 -0400

    breaking commit, update hamburger menu logic

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,19 +1,48 @@
-const handleHamburgerMenu = () => {
-	// Check for hamburger menu existense
-	if (!window.matchMedia("(min-width: 1000px)").matches) {
-		// Get all the anchor menues
-		document.querySelectorAll('a.nav-link').forEach(navLink => {
-			navLink.addEventListener('click', event => {
-				document.getElementById('nav-toggle').checked = false;
-			})
-		})
+import { isMobile } from '../index';
 
-		document.querySelectorAll('a.main-logo').forEach(navLink => {
-			navLink.addEventListener('click', event => {
-				document.getElementById('nav-toggle').checked = false;
-			})
+/**
+ * Funcrtion handles hamburger menu interactions on mobile/small screen devices
+ */
+const handleHamburgerMenu = () => {
+
+	const navBar = document.getElementById('nav-bar');
+	const navLinks = navBar.querySelectorAll('.nav-link');
+
+	// toggle hamburger menue when the hamburger icon is clicked
+	document.querySelector('#nav-toggle').addEventListener('click', event => {
+		toggleHamburgerMenu();
+	})
+
+	// select all the elements that will
+	const navToggleElements = document.querySelectorAll('.nav-link, .main-logo');
+
+	navToggleElements.forEach(element => {
+		element.addEventListener('click', event => {
+			toggleHamburgerMenu(true);
+		})
+	})
+	const toggleHamburgerMenu = (closeOnly = false) => {
+		let newOpacity = 0;
+		if (!closeOnly) {
+			if (navBar.style.transform === 'scale(1, 1)') {
+				newOpacity = 0;
+			}
+			else {
+				newOpacity = 1;
+			}
+		}
+		console.log('newOpacity', newOpacity)
+		navBar.style.transform = `scale(1, ${newOpacity})`;
+
+		navLinks.forEach(element => {
+			element.style.opacity = newOpacity;
+			element.style.transition = 'opacity 250ms ease-in-out 50ms';
 		})
 	}
 };
+
+
+
+
 
 export { handleHamburgerMenu };
