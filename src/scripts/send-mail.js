3c7f25b2d37a34758ef55a56031f15d908f5f25f
Sebastian Shelby <sebastianeshelby@gmail.com>
2023-11-26 01:19:32 -0500

    implement contact form

---
+++ src/scripts/send-mail.js
@@ -0,0 +1,28 @@
+const sendMail = (event, recaptchaToken) => {
+  const contactForm = document.getElementById("contact-form")
+  event.preventDefault();
+
+  const nameInput = contactForm.querySelector('[name="name"]')
+  const emailInput = contactForm.querySelector('[name="email"]')
+  const messageInput = contactForm.querySelector('[name="message"]')
+
+  if (!nameInput.checkValidity() || !emailInput.checkValidity() || !messageInput.checkValidity())
+    return
+
+  contactForm.reset()
+
+  const params = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+    'g-recaptcha-response': recaptchaToken
+  }
+
+  emailjs.send("default_service", "template_25vyieb", params)
+    .then(() => {
+      alert("Thanks for your email!")
+    }).catch(err => {
+      console.error(err)
+      alert(`Something went wrong! ${err.text}`)
+    })
+}
